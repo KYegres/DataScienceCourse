6b56cf3582a5653da1b729e9695e710b6a2fc669
Sergey Khrebtov <sergey.khrebtov@gmail.com>
2020-05-24 18:44:43 +0300

    Clean code of GuessNumber.py

--- module_0/GuessNumber.py
+++ module_0/GuessNumber.py
@@ -1,47 +1,41 @@
-import math
-import random
+import numpy as np
 
 
-def play_game(number, max_number=100):
+def play_game(number, max_number):
+    if number > max_number:
+        print(f'Загаданное число больше максимального. {number} > {max_number}')
+        exit(1)
+
     try_count = 1
     min_predict = 0
     max_predict = max_number + 1
     predict = int(max_number / 2)
-    max_try_count = math.ceil(math.log(max_number, 2))
-    #print(predict, min_predict, max_predict)
+
     while predict != number:
+        # print(predict, min_predict, max_predict)
         if predict > number:
             max_predict = predict
         else:
             min_predict = predict
         predict = int((min_predict + max_predict) / 2)
         try_count += 1
-        if try_count > max_try_count:
-            print('Something went wrong while trying to guess number', number)
-            exit(1)
-        #print(predict, min_predict, max_predict)
+
     return try_count
 
-#print(math.ceil(math.log(100, 2))+1)
-#print('Try count:', play_game(2))
-#exit(0)
 
 def score_game(game):
-    count_ls = []
-    random_array = []
-    random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    game_results = []
     round_count = 1000
     max_guess_number = 100
-    for i in range(1, round_count + 1):
-        random_array.append(random.randint(1, max_guess_number))
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    game_inputs = np.random.randint(1, max_guess_number, size=round_count)
 
-    for number in random_array:
-        count_ls.append(game(number, max_guess_number))
+    for game_input in game_inputs:
+        game_results.append(game(game_input, max_guess_number))
 
-    score = int(sum(count_ls) / len(count_ls))
+    score = int(np.mean(game_results))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return score
 
 
 score_game(play_game)
-
