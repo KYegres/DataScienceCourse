2b8fd19ea5e8440fea412f682942987d9285cd44
Sergey Khrebtov <sergey.khrebtov@gmail.com>
2020-05-24 19:05:59 +0300

    Clean code of GuessNumber.py, added comments

--- module_0/GuessNumber.py
+++ module_0/GuessNumber.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 def play_game(number, max_number):
+    """Функция угадывает переданное число методом двоичного поиска.
+    На вход принимает загаддное и максимально возможное число, возвращает количество попыток"""
     if number > max_number:
         print(f'Загаданное число больше максимального. {number} > {max_number}')
         exit(1)
@@ -12,7 +14,6 @@ def play_game(number, max_number):
     predict = int(max_number / 2)
 
     while predict != number:
-        # print(predict, min_predict, max_predict)
         if predict > number:
             max_predict = predict
         else:
@@ -23,11 +24,12 @@ def play_game(number, max_number):
     return try_count
 
 
-def score_game(game):
+def score_game(game, round_count=1000, max_guess_number=100):
+    """Функция запускает игру много раз со случайными числами для угадывания.
+    Принимает на вход функцию-игру, количество раундов и максимальное число для угадывания.
+    Возвращает среднее количество попыток до угадывания"""
     game_results = []
-    round_count = 1000
-    max_guess_number = 100
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим
     game_inputs = np.random.randint(1, max_guess_number, size=round_count)
 
     for game_input in game_inputs:
